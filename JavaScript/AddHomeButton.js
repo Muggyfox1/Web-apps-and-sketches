c9c13b0e78679025c026691dccf194eb01ccb69e
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-03-30 23:38:15 -0400

    added home button JS script and CSS

---
+++ JavaScript/AddHomeButton.js
@@ -0,0 +1,5 @@
+let HomeButton = document.createElement("a");
+HomeButton.classList.add("HomeButton");
+HomeButton.href = "/index.html";
+HomeButton.innerText = "HomePage";
+document.querySelector("body").append(HomeButton);
