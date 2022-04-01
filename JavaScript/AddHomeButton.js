50f38e001f1682d6172162ad3c643e4d229fd68e
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-04-01 17:12:22 -0400

    home button script takes care of it's own CSS now.

--- JavaScript/AddHomeButton.js
+++ JavaScript/AddHomeButton.js
@@ -1,3 +1,8 @@
+let styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "/CSS/HomeButton.css";
+document.querySelector("head").appendChild(styleLink);
+
 let HomeButton = document.createElement("a");
 HomeButton.classList.add("HomeButton");
 HomeButton.href = "/index.html";
